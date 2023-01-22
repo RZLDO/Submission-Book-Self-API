577a6245884eb1935c817f7a48689ecc2dea44ec
Rzldo <ezas4531@gmail.com>
2023-01-22 15:16:03 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+const {
+  addBooksHandler, getAllBooksHandler, getBooksByIdHandler, editBooksByIdHandler, DeleteBooksById,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBooksByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBooksByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: DeleteBooksById,
+  },
+];
+
+module.exports = routes;
